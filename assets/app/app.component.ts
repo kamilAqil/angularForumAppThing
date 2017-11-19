17736e04a746e6bd6a8956e13dc1b66cf9cd8f4c
Kamil <kaqil91@gmail.com>
2017-11-19 14:46:50 -0800

    looping through the messages from messages component but it is not passing the data up to the main app component

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-
+import {Message} from './messages/message.model';
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html'
 })
 export class AppComponent {
-    message = {
-        content: 'A message',
-        author: 'Kamilo'
-    }
+    // message : Message = new Message('some message','kamilo');
+    // messages: Message[] = [
+    //     new Message('Some message', 'Kamil'),
+    //     new Message('Second message', 'Kamil')
+    // ];
 }
