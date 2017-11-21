632d597ba3abe0827f315c59c8c692cfe740444e
Kamil <kaqil91@gmail.com>
2017-11-20 22:44:51 -0800

    starting angular routing

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppComponent } from "./app.component";
 import {MessageComponent} from './messages/message.component';
 import { MessageListComponent } from './messages/message-list.component'
 import { messageInputComponent } from './messages/message.input.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AuthenticationComponent } from './auth/authentication.component';
+import { HeaderComponent } from './header.component';
 @NgModule({
     declarations: [
         AppComponent,
         MessageComponent,
         MessageListComponent,
-        messageInputComponent
+        messageInputComponent,
+        MessagesComponent,
+        AuthenticationComponent,
+        HeaderComponent
     ],
     imports: [BrowserModule, FormsModule],
     bootstrap: [AppComponent]
