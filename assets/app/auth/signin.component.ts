94303afc24ec252d130d7024061c40ea43f4fba6
Kamil <kaqil91@gmail.com>
2017-11-27 17:44:54 -0800

    hooked up sign in component

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
     selector: 'app-signIn',
@@ -6,5 +7,21 @@ import { Component } from "@angular/core";
 })
 
 export class SignInComponent {
+    myForm: FormGroup;
+
+    onSubmit() {
+        console.log(this.myForm);
+        this.myForm.reset();
+    }
+
+    ngOnInit() {
+        this.myForm = new FormGroup({
+            email: new FormControl(null, [
+                Validators.required,
+                Validators.pattern(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/)
+            ]),
+            password: new FormControl(null, Validators.required),
+        });
+    }
 
 }
